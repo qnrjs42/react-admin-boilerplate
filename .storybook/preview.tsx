24e9f481f3339bce66640e232959cd376d4f770d
qnrjs42 <qnrjs42@naver.com>
2024-03-08 16:27:17 +0900

    set: storybook

---
+++ .storybook/preview.tsx
@@ -0,0 +1,31 @@
+import type { Preview } from '@storybook/react';
+import { QueryClientProvider } from '@tanstack/react-query';
+import React from 'react';
+
+import '../src/globals.css';
+import '../src/index.css';
+import { Toaster } from '../src/shared/shadcn-ui/ui';
+import utilQueryClient from '../src/shared/utils/utilQueryClient';
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export const decorators = [
+  Story => (
+    <QueryClientProvider client={utilQueryClient()}>
+      <Story />
+      <Toaster />
+    </QueryClientProvider>
+  ),
+];
+
+export default preview;
