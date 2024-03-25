3212cae664943842dd2ae058d1d70c8425505d43
qnrjs42 <qnrjs42@naver.com>
2024-03-25 13:41:30 +0900

    add: DetailWidget

--- src/widgets/index.tsx
+++ src/widgets/index.tsx
@@ -1,7 +1,8 @@
 import ContentWidget from './content';
+import DetailWidget from './detail';
 import HeaderWidget from './header';
 import MainWidget from './main';
 import RightWidget from './right';
 import SidebarWidget from './sidebar';
 
-export { SidebarWidget, HeaderWidget, ContentWidget, RightWidget, MainWidget };
+export { SidebarWidget, HeaderWidget, ContentWidget, RightWidget, MainWidget, DetailWidget };
