33f5e2fe7aa78fe43769aeda7ecd8a4d7b84d6bb
qnrjs42 <qnrjs42@naver.com>
2024-03-25 14:35:51 +0900

    update: banner/:id - ui 분리

---
+++ src/features/banner/ui/Form/index.tsx
@@ -0,0 +1,74 @@
+import type { FC } from 'react';
+import type { UseFormReturn } from 'react-hook-form';
+
+import { Button, Form, FormField } from '@shadcn-ui/ui';
+
+import type { BannerFormDto } from '@entities/banner';
+
+import type { FileWithDropzone } from '@typings/common';
+
+import {
+  SharedFormFieldRender,
+  SharedImageFormFieldRender,
+  SharedSwitchFormFieldRender,
+} from '@components';
+
+interface IProps {
+  form: UseFormReturn<BannerFormDto>;
+  files: FileWithDropzone[];
+  setFiles: React.Dispatch<React.SetStateAction<FileWithDropzone[]>>;
+  submitProps: {
+    label: string;
+    onSubmit: (e?: React.BaseSyntheticEvent<object, any, any> | undefined) => Promise<void>;
+  };
+}
+const BannerForm: FC<IProps> = ({ form, files, setFiles, submitProps }) => {
+  return (
+    <Form {...form}>
+      <form>
+        <div className='flex justify-end'>
+          <Button type='button' onClick={submitProps.onSubmit}>
+            {submitProps.label}
+          </Button>
+        </div>
+        <div className='space-y-6'>
+          <FormField
+            name='isShow'
+            control={form.control}
+            render={({ field }) => <SharedSwitchFormFieldRender label='배너 노출' field={field} />}
+          />
+          <FormField
+            name='title'
+            control={form.control}
+            render={({ field }) => (
+              <SharedFormFieldRender label='배너명' type='text' field={field} isRequired />
+            )}
+          />
+          <FormField
+            name='url'
+            control={form.control}
+            render={({ field }) => (
+              <SharedFormFieldRender label='URL' type='text' field={field} isRequired />
+            )}
+          />
+          <FormField
+            name='imageFiles'
+            control={form.control}
+            render={({ field }) => (
+              <SharedImageFormFieldRender
+                label='이미지 선택 (1장)'
+                imgClassName='h-[400px] w-[300px]'
+                field={field}
+                files={files}
+                setFiles={setFiles}
+                isRequired
+              />
+            )}
+          />
+        </div>
+      </form>
+    </Form>
+  );
+};
+
+export default BannerForm;
--- src/pages/admin/banners/banner.tsx
+++ src/pages/admin/banners/banner.tsx
@@ -1,18 +1,11 @@
 import type { FC } from 'react';
 
-import { Button, Form, FormField } from '@shadcn-ui/ui';
-
 import { DetailWidget } from '@widgets/index';
 
 import { useGetBanner, useModifyBannerForm } from '@features/banner/hooks';
+import { BannerForm } from '@features/banner/ui';
 
 import type { IBanner } from '@entities/banner';
-
-import {
-  SharedFormFieldRender,
-  SharedImageFormFieldRender,
-  SharedSwitchFormFieldRender,
-} from '@components';
 
 const AdminBannerPage: FC = () => {
   const { banner, isLoading, isError } = useGetBanner();
@@ -31,50 +24,15 @@ const AdminBannerDetail: FC<IProps> = ({ banner }) => {
   const { form, files, setFiles, onSubmit } = useModifyBannerForm(banner);
 
   return (
-    <Form {...form}>
-      <form>
-        <div className='flex justify-end'>
-          <Button type='button' onClick={onSubmit}>
-            수정하기
-          </Button>
-        </div>
-        <div className='space-y-6'>
-          <FormField
-            name='isShow'
-            control={form.control}
-            render={({ field }) => <SharedSwitchFormFieldRender label='배너 노출' field={field} />}
-          />
-          <FormField
-            name='title'
-            control={form.control}
-            render={({ field }) => (
-              <SharedFormFieldRender label='배너명' type='text' field={field} isRequired />
-            )}
-          />
-          <FormField
-            name='url'
-            control={form.control}
-            render={({ field }) => (
-              <SharedFormFieldRender label='URL' type='text' field={field} isRequired />
-            )}
-          />
-          <FormField
-            name='imageFiles'
-            control={form.control}
-            render={({ field }) => (
-              <SharedImageFormFieldRender
-                label='이미지 선택 (1장)'
-                imgClassName='h-[400px] w-[300px]'
-                field={field}
-                files={files}
-                setFiles={setFiles}
-                isRequired
-              />
-            )}
-          />
-        </div>
-      </form>
-    </Form>
+    <BannerForm
+      form={form}
+      files={files}
+      setFiles={setFiles}
+      submitProps={{
+        label: '수정하기',
+        onSubmit,
+      }}
+    />
   );
 };
 
