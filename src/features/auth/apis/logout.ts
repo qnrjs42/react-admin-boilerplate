2eaabe8d7c3932d720aace2d7dbd7fdd516df2dc
qnrjs42 <qnrjs42@naver.com>
2024-03-26 14:43:48 +0900

    update: auth/logout - axios header authorization 초기화 로직 수정

--- src/features/auth/apis/logout.ts
+++ src/features/auth/apis/logout.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const apiAuthLogout = async (): Promise<void> => {
   await axios.post('/api/auth/logout');
 
-  axios.defaults.headers.common.authorization = '';
+  delete axios.defaults.headers.common.authorization;
 };
 
 export default apiAuthLogout;
