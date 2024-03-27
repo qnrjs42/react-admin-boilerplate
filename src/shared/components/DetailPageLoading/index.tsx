4b2186d8d0cc3857b303899ea29fc7471467cf17
qnrjs42 <qnrjs42@naver.com>
2024-03-27 17:40:50 +0900

    update: DetailPageLoading - api 통신이 1.5초 이상 지속되면 스켈레톤 표시

--- src/shared/components/DetailPageLoading/index.tsx
+++ src/shared/components/DetailPageLoading/index.tsx
@@ -1,18 +1,36 @@
-import type { FC } from 'react';
+import { type FC, useEffect, useState } from 'react';
 
 import { Skeleton } from '@shadcn-ui/ui';
 
 import PageContainer from '@components/PageContainer';
 
 const DetailPageLoading: FC = () => {
+  const [isShowSkeleton, setIsShowSkeleton] = useState<boolean>(false);
+
+  useEffect(() => {
+    const showTimer: NodeJS.Timeout = setTimeout(() => {
+      setIsShowSkeleton(true);
+    }, 1500);
+
+    return (): void => {
+      clearTimeout(showTimer);
+    };
+  }, []);
+
   return (
     <PageContainer className='space-y-6'>
-      <Skeleton className='h-[125px] rounded-lg' />
-      <div className='space-y-2'>
-        <Skeleton className='h-4 w-[85%]' />
-        <Skeleton className='h-4 w-[85%]' />
-        <Skeleton className='h-4 w-[85%]' />
-      </div>
+      {isShowSkeleton ? (
+        <>
+          <Skeleton className='h-[125px] rounded-lg' />
+          <div className='space-y-2'>
+            <Skeleton className='h-4 w-[85%]' />
+            <Skeleton className='h-4 w-[85%]' />
+            <Skeleton className='h-4 w-[85%]' />
+          </div>
+        </>
+      ) : (
+        <div className='h-[200px]' />
+      )}
     </PageContainer>
   );
 };
